110a5e6cef21c7ebd0d8ad291145c66fb1af115f
tegarsatya <59832647+tegarsatya@users.noreply.github.com>
2020-04-18 21:18:41 -1200

    first commit

---
+++ application/views/login/list.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title><?= $title ?></title>
+	<!-- Tell the browser to be responsive to screen width -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="<?= base_url() ?>assets/admin/plugins/fontawesome-free/css/all.min.css">
+	<!-- Ionicons -->
+	<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+	<!-- icheck bootstrap -->
+	<link rel="stylesheet" href="<?= base_url() ?>assets/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+	<!-- Theme style -->
+	<link rel="stylesheet" href="<?= base_url() ?>assets/admin/dist/css/adminlte.min.css">
+	<!-- Google Font: Source Sans Pro -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+
+<body class="hold-transition login-page">
+	<div class="login-box">
+		<div class="login-logo">
+			<a href=""><b>Halaman</b>Login</a>
+		</div>
+		<!-- /.login-logo -->
+		<div class="card">
+			<div class="card-body login-card-body">
+				<p class="login-box-msg">Sign in to start your session</p>
+				<?php
+				// Notifikasi error
+				echo validation_errors('<p class="alert alert-warning">', '</p>');
+
+				// Form open 
+				echo form_open(base_url('login'));
+				?>
+				<div class="input-group mb-3">
+					<input type="text" name="username" class="form-control" placeholder="Username">
+				</div>
+				<div class="input-group mb-3">
+					<input type="password" name="password" class="form-control" placeholder="Password">
+				</div>
+				<div class="row">
+					<div class="col-8">
+						<div class="icheck-primary">
+							<input type="checkbox" id="remember">
+							<label for="remember">
+								Remember Me
+							</label>
+						</div>
+					</div>
+					<!-- /.col -->
+					<div class="col-4">
+						<button type="submit" class="btn btn-primary btn-block">Sign In</button>
+					</div>
+					<!-- /.col -->
+				</div>
+				<?php echo form_close(); ?>
+				<div class="social-auth-links text-center mb-3">
+					<p>- OR -</p>
+					<a href="#" class="btn btn-block btn-primary">
+						<i class="fab fa-facebook mr-2"></i> Sign in using Facebook
+					</a>
+					<a href="#" class="btn btn-block btn-danger">
+						<i class="fab fa-google-plus mr-2"></i> Sign in using Google+
+					</a>
+				</div>
+				<!-- /.social-auth-links -->
+
+				<p class="mb-1">
+					<a href="forgot-password.html">I forgot my password</a>
+				</p>
+				<p class="mb-0">
+					<a href="register.html" class="text-center">Register a new membership</a>
+				</p>
+			</div>
+			<!-- /.login-card-body -->
+		</div>
+	</div>
+	<!-- /.login-box -->
+	<?php if ($this->session->flashdata('sukses')) { ?>
+		<script>
+			swal("Berhasil", "<?php echo $this->session->flashdata('sukses'); ?>", "success")
+		</script>
+	<?php } ?>
+
+	<?php if ($this->session->flashdata('warning')) { ?>
+		<script>
+			swal("Oops...", "<?php echo $this->session->flashdata('warning'); ?>", "warning")
+		</script>
+	<?php } ?>
+
+
+	<!-- jQuery -->
+	<script src="<?= base_url() ?>assets/admin/plugins/jquery/jquery.min.js"></script>
+	<!-- Bootstrap 4 -->
+	<script src="<?= base_url() ?>assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<!-- AdminLTE App -->
+	<script src="<?= base_url() ?>assets/admin/dist/js/adminlte.min.js"></script>
+
+</body>
+
+</html>
